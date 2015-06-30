830623db3f2ce9fbcbca45ee23f765065b6255ca
Graham Smith <gablaxian@users.noreply.github.com>
2015-06-30 13:53:54 +0100

    Several enhancements made
    - The code now internally identifies as miniQuery instead of 'two'.
    - Externally, the function is referenced with _() instead of $2().
    - New get() method for returning the HTMLElement of a specified array element.
    - addClass() and removeClass() both now use classList() for supporting browsers and also work over the entire array.
    - new offset() function for getting an element's position in the document.
    - other minor fixes and improvements

--- miniQuery.js
+++ miniQuery.js
@@ -1,21 +1,22 @@
 /**
     MiniQuery
-
     Useful for fast DOM interaction without incurring a full jQuery overhead.
-    Takes a class name and only returns 1 element.
-
+    Made with loads of help from http://tech.pro/blog/1385/out-growing-jquery
     Author - Graham Smith
 **/
 ;(function(undefined) {
     'use strict';
     
+    // internal variables and functions
     var slice = Array.prototype.slice,
         forEach = function(array,callback){
-            var i, length = array.length;
-            for(i = 0; i < length; i++)
+            var i = 0;
+            var length = array.length;
+            for (; i < length; i++) {
                 callback.call( array[i], i, array[i] );
+            }
         },
-        isTwo = function(d) {return d instanceof $2;},
+        isMini = function(d) {return d instanceof _;},
         isElement = function(o) {
             return (
                 typeof HTMLElement === "object" ? o instanceof HTMLElement : //DOM2
@@ -23,11 +24,11 @@
             );
         };
     
-    var two = function(selector) {
-        if(!isTwo(this)){
-            return new two(selector);
+    var miniQuery = function(selector) {
+        if(!isMini(this)){
+            return new miniQuery(selector);
         }
-        var self    = this, nodes;
+        var self = this, nodes;
 
         if(isElement(selector)){
             //element was passed in...  shortcut this case.
@@ -44,28 +45,116 @@
         self.length = nodes.length;
     };
 
-    two.fn = two.prototype;
+    miniQuery.fn = miniQuery.prototype;
 
-    two.fn.slice = slice;
+    miniQuery.fn.slice = slice;
 
-    two.fn.each = function(callback) {
+    miniQuery.fn.each = function(callback) {
         forEach(this, callback);
         return this;
     }
 
-    two.fn.first        = function() { return new two(this[0]); }
-    two.fn.css          = function(s, t) {
+    /*
+        get()
+
+        Returns the HTMLElement for calling basic JS on the object.
+
+        e.g: _('.class').get().toLowerCase();
+    */
+    miniQuery.fn.get = function(idx) {
+        if( !idx || parseInt(idx) === 0 ) {
+            return this[0];
+        }
+        else {
+            return this[idx];
+        }
+    }
+
+    /*
+        first()
+
+        Returns the first element in the array (as a miniQuery object)
+    */
+    miniQuery.fn.first = function() {
+        return new miniQuery(this[0]);
+    }
+
+    /*
+        css()
+
+        *Only works on the first element in the array*
+
+        Either returns an css property value, or allows you to set a css property on an element.
+        Uses non jQuery css class nomenclature, so 'background-image' becomes 'backgroundImage'. General rule is remove the dash and then camelCase.
+    */
+    miniQuery.fn.css = function(s, t) {
         if(arguments.length == 2) {
-            for(var i = this.length - 1; i >= 0; --i) { this[i].style[s] = t; };
+            for(var i = this.length - 1; i >= 0; --i) {
+                this[i].style[s] = t;
+            };
             return this;
         }
         else {
             return window.getComputedStyle(this[0])[s];
         }
     }
-    two.fn.addClass     = function (c) { arr = this.elm.getAttribute('class').replace(/[\s]{2}/, ' ').split(' '); arr.push(c); this.elm.setAttribute('class', arr.join(' ')); }
-    two.fn.removeClass  = function (c) { arr = this.elm.getAttribute('class').replace(/[\s]{2}/, ' ').split(' '); idx = arr.indexOf(c); if(idx != -1) { arr.splice(idx); this.elm.setAttribute('class', arr.join(' ')); } }
+
+    /*
+        addClass()
+
+        Adds the specified class to all elements in the array
+    */
+    miniQuery.fn.addClass = function (className) {
+        var i = 0;
+        var length = this.length;
+        if (this[0].classList) {
+            for (; i < length; i++) {
+                this[i].classList.add(className);
+            }
+        }
+        else {
+            for (; i < length; i++) {
+                this[i].className += ' ' + className;
+            }
+        }
+    }
+
+    /*
+        addClass()
+
+        Removes the specified class to all elements in the array
+    */
+    miniQuery.fn.removeClass = function (className) {
+        var i = 0;
+        var length = this.length;
+        if (this[0].classList) {
+            for (; i < length; i++) {
+                this[i].classList.remove(className);
+            }
+        }
+        else {
+            for (; i < length; i++) {
+                this[i].className = this[i].className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+            }
+        }
+    }
+
+    /*
+        offset()
+
+        *Only works on the first element in the array*
+
+        Gets the position of the element within the document.
+    */
+    miniQuery.fn.offset = function() {
+        var rect = this[0].getBoundingClientRect();
+
+        return {
+          top: rect.top + document.body.scrollTop,
+          left: rect.left + document.body.scrollLeft
+        }
+    }
 
     //export constructors
-    window.$2 = window.$2 || two;
+    window._ = window._ || miniQuery;
 }());
